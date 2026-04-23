cec1e680495a631e650fcce8a124f49a05230125
makscebotnikov-cmd <makscebotnikov@gmail.com>
2026-04-23 12:33:40 +0300

    variables.tf

---
+++ src/vpc/variables.tf
@@ -0,0 +1,16 @@
+variable "env_name" {
+  type = string
+}
+
+variable "zone" {
+  type = string
+}
+
+variable "cidr" {
+  type = string
+}
+
+variable "network_name" {
+  type    = string
+  default = ""
+}
